c7130ba4e26c32b20922b09042a8cc5ae1453fc8
Manav Mahesh Sanger <communicate.manav@gmail.com>
2024-05-23 17:26:58 +0530

    basic frame complete

---
+++ src/components/OnBoarding.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function OnBoarding() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default OnBoarding;
---
+++ src/components/UserProfile.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import prisma from "@/db";
+import {auth} from "@/auth";
+import {redirect} from "next/navigation";
+import OnBoarding from "@/components/OnBoarding";
+
+async function UserProfile() {
+    const session = await auth()
+    const myUser = await prisma.users.findUnique({
+        where: { email: session?.user?.email! },
+    });
+    const role = myUser?.role;
+    return (
+        <div className="">
+            {!role &&  <OnBoarding/>}
+        </div>
+    );
+}
+
+export default UserProfile;
